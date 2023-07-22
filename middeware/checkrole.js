3a758b141790d811b45d0a5d5cf1531b518e08a9
ponnaganti963 <ponnagantimanikanta26@gmail.com>
2023-07-22 12:45:37 +0530

    seond commit

--- middeware/checkrole.js
+++ middeware/checkrole.js
@@ -3,11 +3,16 @@
 const UserModel = require('../models/user')
 
 const checkRole = (roles) => async (req, res, next) => {
-    let { email } = req.body
+    try{
 
-    const user = await UserModel.findOne({ email: email})
-
-    !roles.includes(user.role) ? res.status(401).json("Sorry you don't have access") : next()
+        let { email } = req.body
+    
+        const user = await UserModel.findOne({ email: email})
+    
+        !roles.includes(user.role) ? res.status(401).json("Sorry you don't have access") : next()
+    }catch(err) {
+        res.status(400).json({message: err.message})
+    }
 }
 
 module.exports = checkRole
--- routes/routes.js
+++ routes/routes.js
@@ -7,7 +7,7 @@ const bcrypt = require('bcrypt')
 const BookModel = require('../models/book')
 const UserModel = require('../models/user')
 
-router.post('/book', checkRole(['admin']), async (req, res) => {
+router.post('/books', checkRole(['admin']), async (req, res) => {
   
     const newBook = new BookModel({ 
         title: req.body.title,
@@ -25,11 +25,34 @@ router.post('/book', checkRole(['admin']), async (req, res) => {
     }
 })
 
+router.get('/books/:id', async (req, res) => {
+    try{
+        let book = await BookModel.findById(req.params.id)
+        res.status(200).json(book)
+    }catch(err){
+        res.status(404).json({message: `Book With Id ${req.params.id} is not found`})
+    }
+})
 
-// const get 
+router.delete('/books/:id', checkRole(['admin']), async (req, res) => {
+    try{
+        let book = await BookModel.findByIdAndDelete(req.params.id)
+        if(book)
+        res.status(200).json({message: `Book with Id ${req.params.id} is successfully deleted`, book: book})
+        else
+        res.status(404).json({message: `Book with Id ${req.params.id} is not found`})
+    }catch(err){
+        res.status(404).json({message: `Book with Id ${req.params.id} is not found`})
+    }
+})
 
-router.get('/book',  async (req, res) => {
-    res.send('Get API')
+router.get('/books',  async (req, res) => {
+    try{
+        let books = await BookModel.find() 
+        res.status(200).json({books: books})
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
 })
 
 router.post('/auth/signup', async (req, res) => {
@@ -55,6 +78,22 @@ router.post('/auth/signup', async (req, res) => {
         return res.status(200).json({message: "User added successfully!!"})
     }catch(error){
         return res.status(500).json({message: error.message})
+    }
+})
+
+router.post('/auth/login', async (req, res) => {
+    try{
+        console.log(req.body.email)
+        let user = await UserModel.findOne({email : req.body.email})
+        
+        if(!user) return res.status(404).json({message: 'User Not Found'})
+
+        let isPasswordValid = bcrypt.compareSync(req.body.password, user.password) 
+        if(!isPasswordValid) return res.status(401).json({message: 'Invalid password'})
+        return res.status(200).json({message: 'Login Successfull!'})
+
+    }catch(error){
+        return res.status(500).json({message: "User not found"})
     }
 })
 
